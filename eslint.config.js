1329e2f5b29e5b2b0e1e43e8c0ab8cc2e165f20b
dinwwwh <dinwwwh@gmail.com>
2024-07-26 16:59:44 +0700

    chore: init

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu(
+    {
+        react: true,
+        ignores: ['**/*.gen.ts'],
+        formatters: {
+            css: true,
+            html: true,
+            markdown: true,
+        },
+    },
+    {
+        rules: {
+            'react/prefer-destructuring-assignment': ['off'],
+            'react-dom/no-missing-button-type': ['off'],
+            'react-refresh/only-export-components': ['off'],
+        },
+    }
+);
