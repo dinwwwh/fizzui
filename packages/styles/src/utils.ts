01dca1299f89be61577c05de4348f0ea1107d6f2
dinwwwh <dinwwwh@gmail.com>
2024-08-03 15:48:36 +0700

    refactor(styles): improve reusable of selectors

--- packages/styles/src/utils.ts
+++ packages/styles/src/utils.ts
@@ -1,6 +1,6 @@
-export const disabledSelector = '&:disabled, &[aria-disabled=\'\'], &[aria-disabled=\'true\'], &[data-disabled=\'\'], &[data-disabled=\'true\']'
+export const disabledSelector = '&:is(:disabled, [aria-disabled=\'\'], [aria-disabled=\'true\'], [data-disabled=\'\'], [data-disabled=\'true\'])'
 export const disabledWithinSelector = '&:has(:disabled, [aria-disabled=\'\'], [aria-disabled=\'true\'], [data-disabled=\'\'], [data-disabled=\'true\'])'
-export const userInvalidSelector = '&:user-invalid, &[aria-invalid=\'\'], &[aria-invalid=\'true\'], &[data-invalid=\'\'], &[data-invalid=\'true\']'
+export const userInvalidSelector = '&:is(:user-invalid, [aria-invalid=\'\'], [aria-invalid=\'true\'], [data-invalid=\'\'], [data-invalid=\'true\'])'
 export const userInvalidWithinSelector = '&:has(:user-invalid, [aria-invalid=\'\'], [aria-invalid=\'true\'], [data-invalid=\'\'], [data-invalid=\'true\'])'
 export const focusSelector = '&:focus'
 export const focusVisibleSelector = '&:focus-visible'
@@ -9,7 +9,7 @@ export const focusVisibleWithinSelector = '&:focus-within:has(:focus-visible)'
 export const focusVisibleInvalidWithinSelector = '&:focus-within:has(:focus-visible):has(:invalid, [aria-invalid=\'\'], [aria-invalid=\'true\'], [data-invalid=\'\'], [data-invalid=\'true\'])'
 export const focusVisibleUserInvalidWithinSelector = '&:focus-within:has(:focus-visible):has(:user-invalid, [aria-invalid=\'\'], [aria-invalid=\'true\'], [data-invalid=\'\'], [data-invalid=\'true\'])'
 export const hoverSelector = '&:is(:hover, [data-hover=\'\'], [data-hover=\'true\']):not(:disabled, [aria-disabled=\'\'], [aria-disabled=\'true\'], [data-disabled=\'\'])'
-export const pressedSelector = '&:active:not(:disabled, [aria-disabled=\'\'], [aria-disabled=\'true\'], [data-disabled=\'\'], [data-disabled=\'true\']), &[data-pressed=\'true\']:not(:disabled, [aria-disabled=\'\'], [aria-disabled=\'true\'], [data-disabled=\'\'], [data-disabled=\'true\'])'
+export const pressedSelector = '&:is(:active, [data-pressed=\'\'] ,[data-pressed=\'true\']):not(:disabled, [aria-disabled=\'\'], [aria-disabled=\'true\'], [data-disabled=\'\'], [data-disabled=\'true\'])'
 export const enabledSelector = '&:not(:disabled, [aria-disabled=\'\'], [aria-disabled=\'true\'], [data-disabled=\'\'], [data-disabled=\'true\'])'
 
 export function rem(px: number) {
